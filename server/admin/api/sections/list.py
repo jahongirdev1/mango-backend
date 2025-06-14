5baea9235a236ae395fb85bd2e4334ec900189ef
mirzafar <m_mirzafar@mail.ru>
2025-06-14 21:11:39 +0500

    fix

--- server/admin/api/sections/list.py
+++ server/admin/api/sections/list.py
@@ -1,5 +1,6 @@
 from core.db import db
 from core.handlers import BaseAPIView
+from core.tools import set_counters
 from utils.ints import IntUtils
 from utils.lists import ListUtils
 from utils.strs import StrUtils
@@ -9,13 +10,25 @@ class SectionsView(BaseAPIView):
     template_name = 'admin/sections.html'
 
     async def get(self, request, user):
+        if user['branch_id']:
+            branch_id = user['branch_id']
+        else:
+            branch_id = IntUtils.to_int(request.args.get('branch_id'))
+
+        cond, cond_vars = ['is_active'], []
+        if branch_id:
+            cond.append('branch_id = {}')
+            cond_vars.append(branch_id)
+
+        cond, _ = set_counters(' AND '.join(cond))
         items = ListUtils.to_list_of_dicts(await db.fetch(
             '''
-            SELECT id, title, photo, position
+            SELECT id, title, photo, position, branch_id
             FROM control.sections
-            WHERE is_active
+            WHERE %s
             ORDER BY id DESC
-            '''
+            ''' % cond,
+            *cond_vars
         ))
 
         return self.success(request=request, user=user, data={
@@ -27,18 +40,27 @@ class SectionsView(BaseAPIView):
         photo = StrUtils.to_str(request.json.get('photo'))
         position = IntUtils.to_int(request.json.get('position'))
 
+        if user['branch_id']:
+            branch_id = user['branch_id']
+        else:
+            branch_id = IntUtils.to_int(request.json.get('branch_id'))
+
         if not title:
             return self.error(message='Отсуствует обязательный параметры "Имя"')
 
+        if not branch_id:
+            return self.error(message='Отсуствует обязательный параметры "Филиал"')
+
         inserted_id = await db.fetchval(
             '''
-            INSERT INTO control.sections (title, photo, position)
-            VALUES ($1, $2, $3)
+            INSERT INTO control.sections (title, photo, position, branch_id)
+            VALUES ($1, $2, $3, $4)
             RETURNING id
             ''',
             title,
             photo,
-            position
+            position,
+            branch_id
         )
 
         if not inserted_id:
