0bc3b4858ce6a59affb0e81e843afb589599ff02
mirzafar <m_mirzafar@mail.ru>
2025-06-28 17:07:36 +0500

    fix items

--- server/bridge/api/sections/list.py
+++ server/bridge/api/sections/list.py
@@ -13,9 +13,17 @@ class SectionsBridgeView(TemplateHTTPView):
     async def get(self, request):
         cond, cond_vars = ['is_active'], []
         branch_id = IntUtils.to_int(request.args.get('branch_id'))
+        parent_id = IntUtils.to_int(request.args.get('parent_id'))
+
         if branch_id:
             cond.append('branch_id = {}')
             cond_vars.append(branch_id)
+
+        if parent_id:
+            cond.append('parent_id = {}')
+            cond_vars.append(parent_id)
+        else:
+            cond.append('parent_id IS NULL')
 
         cond, _ = set_counters(' AND '.join(cond))
         items = ListUtils.to_list_of_dicts(await db.fetch(
